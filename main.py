7775fe02c23f55a052cfb0fbc6720cfc75c13e19
ikbal-nayem <ikbalnayem000@gmail.com>
2026-01-14 11:50:33 +0600

    Allow CORS *

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from datetime import datetime
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from middlewares.CORS import origins
 
 from services.bangladesh_holidays import getHolidaysJSON
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/get-bd-gov-holidays", status_code=status.HTTP_200_OK)
 async def getGovHolidays(year: int = datetime.now().year):
